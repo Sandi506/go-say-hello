869c7e7f1d9c3ac3dca35a5278399632e1ff45f5
Sandi506 <sandypermanaputra151@gmail.com>
2022-04-04 21:53:12 +0700

    first commit

---
+++ say_hello.go
@@ -0,0 +1,11 @@
+package go_say_hello
+
+// nama adalah parameter
+// return untuk type string
+
+func SayHello(nama string) string {
+	return "Hello " + nama
+}
+func Kali(a int, b int) int {
+	return a * b
+}
